27b6765e5fdeb2e7b2907fd91d6e86fbca479b47
Clément Rault <70482294+dig2root@users.noreply.github.com>
2023-03-12 15:53:19 -0400

    Initial commit

---
+++ include/CustomImGuiFrame.h
@@ -0,0 +1,10 @@
+#include "ImGuiFrame.h"
+
+class CustomImGuiFrame : public ImGuiFrame {
+public:
+	virtual void Update() override;
+
+private:
+	float clear_color[3] = { .0f, .0f, .0f };
+	void change_clear_color(float r, float g, float b);
+};
